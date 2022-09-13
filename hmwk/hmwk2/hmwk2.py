c4c3a114a51c711682d961a9fdb3e7d5788d15e6
Robert David Hernandez <rherna70@uic.edu>
2022-09-12 19:50:39 -0500

    script skeleton

---
+++ hmwk/hmwk2/hmwk2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Perception:
+    def __init__(self):
+        pass
+    def predict(self, x):
+        pass
+    
+def perceptron_training_algorithm(perceptron, data):
+    pass
+
+def organize_data():
+    pass
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
+
+# EOF
