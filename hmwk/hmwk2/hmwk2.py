b285e81608448ca6963fdb1606b464ced5ea8e76
Robert David Hernandez <rherna70@uic.edu>
2022-09-12 19:52:29 -0500

    math notation for perceptron training algorithm

--- hmwk/hmwk2/hmwk2.py
+++ hmwk/hmwk2/hmwk2.py
@@ -1,6 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+# from lecture notes
+#  -  We can write the Algo a bit more simply:
+    # 1. Init 𝛺 randomly
+    # 2. While 𝛺 cannot correctly classify all input patterns, i.e. while u(𝛺Txi) ≠ di for some i ∈ {1, … , n}
+    # 	a) For i = 1 to n 
+    # 		i) 𝛺 ← 𝛺 + 𝜂xi (di - u(𝛺Txi)) 
+
+
 class Perception:
     def __init__(self):
         pass
