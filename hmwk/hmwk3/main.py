31536b4961f5fad9ce6015b058238c75b7792705
Robert David Hernandez <rherna70@uic.edu>
2022-09-22 17:31:22 -0500

    we don't really need getters/setters in python

--- hmwk/hmwk3/main.py
+++ hmwk/hmwk3/main.py
@@ -1,9 +1,10 @@
+from tkinter import W
 import numpy as np
 import matplotlib.pyplot as plt
 from mnist import MNIST
 
 
-def PerceptronNode():
+class PerceptronNode():
     def __init(self, weights, bias):
         self.weights = weights
         self.bias = bias
@@ -11,71 +12,42 @@ def PerceptronNode():
     def get_output(self, inputs):
         return np.dot(self.weights, inputs) + self.bias
 
-    def get_weights(self):
-        return self.weights
-    
-    def get_bias(self):
-        return self.bias
-
-    def set_weights(self, weights):
-        self.weights = weights
-    
-    def set_bias(self, bias):
-        self.bias = bias
-
-def PerceptronLayer():
+class PerceptronLayer():
     def __init__(self, num_nodes, num_inputs):
         self.num_nodes = num_nodes
         self.num_inputs = num_inputs
         self.nodes = [PerceptronNode() for i in range(num_nodes)]
         for node in self.nodes:
             node.set_weights(np.random.rand(num_inputs))
-            node.set_bias(np.random.rand())
-
-    def get_output(self, inputs):
-        return [node.get_output(inputs) for node in self.nodes]
-
-    def get_weights(self):
-        return [node.get_weights() for node in self.nodes]
-
-    def get_bias(self):
-        return [node.get_bias() for node in self.nodes]
-
-    def set_weights(self, weights):
-        for i in range(self.num_nodes):
-            self.nodes[i].set_weights(weights[i])
-
-    def set_bias(self, bias):
-        for i in range(self.num_nodes):
-            self.nodes[i].set_bias(bias[i])
+            # for this case we will ignore the bias
+            # node.set_bias(np.random.rand())
+            node.set_bias(0)
 
 
-def PerceptronNetwork():
-    def __init__(self, num_layers, num_nodes, num_inputs):
+class PerceptronNetwork():
+    def __init__(self, num_layers, num_input_nodes, num_inputs, num_outputs):
         self.num_layers = num_layers
-        self.num_nodes = num_nodes
+        self.num_nodes = num_input_nodes
         self.num_inputs = num_inputs
-        self.layers = [PerceptronLayer(num_nodes, num_inputs) for i in range(num_layers)]
-        for layer in self.layers:
-            layer.set_weights(np.random.rand(num_nodes, num_inputs))
-            layer.set_bias(np.random.rand(num_nodes))
 
-    def get_output(self, inputs):
-        return [layer.get_output(inputs) for layer in self.layers]
+        # we actually want a network with 784 input nodes and 10 output nodes for this specific case
+        layer_one = PerceptronLayer(num_input_nodes, num_inputs)
+        layer_two = PerceptronLayer(num_outputs, num_inputs)
+        self.layers = [layer_one, layer_two]
 
-    def get_weights(self):
-        return [layer.get_weights() for layer in self.layers]
+        # we actually init weights in the PerceptronLayer constructor
+        # self.layers = [PerceptronLayer(num_nodes, num_inputs) for i in range(num_layers)]
+        # for layer in self.layers:
+        #     layer.set_weights(np.random.rand(num_nodes, num_inputs))
+        #     layer.set_bias(np.random.rand(num_nodes))
 
-    def get_bias(self):
-        return [layer.get_bias() for layer in self.layers]
 
-    def set_weights(self, weights):
-        for i in range(self.num_layers):
-            self.layers[i].set_weights(weights[i])
+def step_function(x) -> int:
+    return 1 if x >= 0 else 0
 
-    def set_bias(self, bias):
-        for i in range(self.num_layers):
-            self.layers[i].set_bias(bias[i])
+
+def arr_step_function(x) -> np.ndarray:
+    return np.array([step_function(xi) for xi in x])
 
 
 def main():
@@ -90,6 +62,17 @@ def main():
     images, labels = mndata.load_training()
 
     # testing_images, testing_labels = mndata.load_testing()
+
+    num_layers = 2
+    num_input_nodes, num_inputs = 784, 784
+    num_outputs = 10
+    perceptron_network = PerceptronNetwork(num_layers, num_input_nodes, num_inputs, num_outputs)
+
+    w = np.random.uniform(-1, 1, size=(10, 784))
+    epoch = 0
+    eta = 0.1
+    episilon = 0.0
+    n = 1000
 
     # should render a 2 
     print(mndata.display(images[5]))
