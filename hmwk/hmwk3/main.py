cc12a4fb12a9cddc0e951eb2a3f9d503b4928199
Robert David Hernandez <rherna70@uic.edu>
2022-09-22 16:29:27 -0500

    context notes

--- hmwk/hmwk3/main.py
+++ hmwk/hmwk3/main.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as plt
 from mnist import MNIST
 
 
+def foo():
+    pass
+
 def main():
 
     mndata = MNIST('resources')
 
     # each image of the images list is a python list of unsigned bytes
+    # In the images: "0" is white, "255" is black, we see in the python-mnist source code library that >200 is dark enough to be considered writing
+    # each image is 28x28 pixels
+
     # labels is a python array of insigned bytes, luckily python translates these to ints no problem
     images, labels = mndata.load_training()
 
