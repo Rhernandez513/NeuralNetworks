24a71bd6a080f88a3098fc921a60dc141899c8aa
Robert David Hernandez <rherna70@uic.edu>
2022-10-20 13:00:00 -0500

    update loading logic so training and testing datasets are correctly seperated and intermixed.  Each training set contains 8000 of each class and each test set contains 2000 of each class

--- hmwk/hmwk5/main.py
+++ hmwk/hmwk5/main.py
@@ -24,39 +24,50 @@ def main():
 
     # section 1, read images into tensor files
     file_names = get_filenames(geometry_dataset_path)
-    # file_names.sort()
 
-    images = []
+    class_names = set()
     for file_name in file_names:
-        img = read_image(geometry_dataset_path + file_name)
-        images.append(img)
+        class_name = file_name[:file_name.find('_')]
+        class_names.add(class_name)
 
-    count = len(images)
+    file_names_by_class = {
+        class_name: [] for class_name in class_names
+    }
 
-    images = torch.stack(images)
+    for file_name in file_names:
+        for class_name in class_names:
+            if file_name.startswith(class_name):
+                file_names_by_class[class_name].append(file_name)
 
     # seperate images into training and testing
     # first 80% for training, last 20% for testing
-    divider = int(count * 0.8)
-    training_tensor = images[:divider]
-    test_tensor = images[divider:]
+    training_files_by_class = {
+        class_name: file_names_by_class[class_name][:8000] for class_name in class_names
+    }
 
-    # torch.save(training_tensor, "training_tensor.file")
-    # torch.save(test_tensor, "test_tensor.file")
+    testing_files_by_class = {
+        class_name: file_names_by_class[class_name][8000:] for class_name in class_names
+    }
+
+    training_images = []
+    testing_images = []
+    for key, value in training_files_by_class.items():
+        for file_name in value:
+            training_images.append(read_image(geometry_dataset_path + file_name))
+    for key, value in testing_files_by_class.items():
+        for file_name in value:
+            testing_images.append(read_image(geometry_dataset_path + file_name))
+    training_tensor = torch.stack(training_images)
+    test_tensor = torch.stack(testing_images)
+
+    torch.save(training_tensor, "training_tensor.file")
+    torch.save(test_tensor, "test_tensor.file")
 
     loaded_training_tensor = torch.load("training_tensor.file")
     loaded_test_tensor = torch.load("test_tensor.file")
 
-    loaded_training_tensor = torch.stack(loaded_training_tensor)
-    loaded_test_tensor = torch.stack(loaded_test_tensor)
-
-    # print(torch.eq(loaded_test_tensor, test_tensor))
-    # print(torch.equal(loaded_test_tensor, test_tensor))
-    # print(torch.eq(loaded_training_tensor, training_tensor))
-    # print(torch.equal(loaded_training_tensor, training_tensor))
-
-    print(torch.all(torch.eq(loaded_test_tensor, test_tensor)))
-    print(torch.all(torch.eq(loaded_training_tensor, training_tensor)))
+    # section 2, train the model
+    # section 3, test the model
 
     print("done")
 
