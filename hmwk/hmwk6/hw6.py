fcb23444442735e2415735455a7eeff37c39bf32
Robert David Hernandez <rherna70@uic.edu>
2022-11-04 18:16:57 -0500

    got something running to completion

--- hmwk/hmwk6/hw6.py
+++ hmwk/hmwk6/hw6.py
@@ -16,6 +16,8 @@ from torch import nn
 import torch.nn.functional as F
 import torch.optim as optim
 from sklearn.manifold import TSNE
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import accuracy_score
 import plotly.express as px
 
 
@@ -52,7 +54,7 @@ test_dataset.transform = test_transform
 m=len(train_dataset)
 
 #random_split randomly split a dataset into non-overlapping new datasets of given lengths
-#train (55,000 images), val split (5,000 images)
+#train (48,000 images), val split (5,000 images)
 train_data, val_data = random_split(train_dataset, [int(m-m*0.2), int(m*0.2)])
 
 batch_size=256
@@ -294,74 +296,96 @@ noise_factor = 0.3
 num_epochs = 30
 history_da={'train_loss':[],'val_loss':[]}
 
-# generate_images
+count = 9
+randomly_selected_val_imgs = [random.randint(0,len(val_data)) for i in range(count)]
 
-# def generate_images(data_loader,encoder,decoder,noise_factor=0.3):
-def generate_images():
-    count = 9
-    r = [random.randint(0,len(val_data)) for i in range(count)]
+
+# for epoch in range(num_epochs):
+#     print('EPOCH %d/%d' % (epoch + 1, num_epochs))
+#     ### Training (use the training function)
+#     train_loss=train_epoch_den(
+#         encoder=encoder, 
+#         decoder=decoder, 
+#         device=device, 
+#         dataloader=train_loader, 
+#         loss_fn=loss_fn, 
+#         optimizer=optim,noise_factor=noise_factor)
+#     ### Validation  (use the testing function)
+#     val_loss = test_epoch_den(
+#         encoder=encoder, 
+#         decoder=decoder, 
+#         device=device, 
+#         dataloader=valid_loader, 
+#         loss_fn=loss_fn,noise_factor=noise_factor)
+#     # Print Validationloss
+#     history_da['train_loss'].append(train_loss)
+#     history_da['val_loss'].append(val_loss)
+#     print('\n EPOCH {}/{} \t train loss {:.3f} \t val loss {:.3f}'.format(epoch + 1, num_epochs,train_loss,val_loss))
+#     # plot_ae_outputs_den(encoder,decoder,noise_factor=noise_factor)
+
+# derived from both online examples of MNIST kmeans clustering 
+# combined with github copilot suggestions
+def retrieve_info(cluster_labels, actual_labels):
+    ref_labels = {}
+    for i in range(len(np.unique(kmeans.labels_))):
+        index = np.where(cluster_labels == i, 1, 0)
+        num = np.bincount(actual_labels[index == 1]).argmax()
+        ref_labels[i] = num
+    return ref_labels
+
+# put your image generator here
+def generate_images() -> tuple:
+    unsqueezed_images = []
+    og_squeezed_images = []
 
     for i in range(count):
-        img = val_data[r[i]][0].unsqueeze(0)
+        og_squeezed_images.append([val_data[randomly_selected_val_imgs[i]][0]])
+        img = val_data[randomly_selected_val_imgs[i]][0].unsqueeze(0)
+        unsqueezed_images.append(img)
         ax = plt.subplot(3, 3, i+1)
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)  
         plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')  
         plt.axis('off')
     plt.show()
-    print()
+    actual_labels = [val_data[randomly_selected_val_imgs[i]][1] for i in range(count)]
+    return (unsqueezed_images, og_squeezed_images, actual_labels)
 
-generate_images()
-
-for epoch in range(num_epochs):
-    print('EPOCH %d/%d' % (epoch + 1, num_epochs))
-    ### Training (use the training function)
-    train_loss=train_epoch_den(
-        encoder=encoder, 
-        decoder=decoder, 
-        device=device, 
-        dataloader=train_loader, 
-        loss_fn=loss_fn, 
-        optimizer=optim,noise_factor=noise_factor)
-    ### Validation  (use the testing function)
-    val_loss = test_epoch_den(
-        encoder=encoder, 
-        decoder=decoder, 
-        device=device, 
-        dataloader=valid_loader, 
-        loss_fn=loss_fn,noise_factor=noise_factor)
-    # Print Validationloss
-    history_da['train_loss'].append(train_loss)
-    history_da['val_loss'].append(val_loss)
-    print('\n EPOCH {}/{} \t train loss {:.3f} \t val loss {:.3f}'.format(epoch + 1, num_epochs,train_loss,val_loss))
-    plot_ae_outputs_den(encoder,decoder,noise_factor=noise_factor)
-
-
-# put your image generator here
-# TODO migrate image gen code 
-
-
-# this script will accomplish the following:
-# generte 9 random images of digits arranges in a 3x3 matrix
-# labels are not important, include the images in the repor
+unsqueezed_images, og_squeezed_images, actual_labels = generate_images()
 
 # put your clustering accuracy calculation here
 
+# we must cluster all 48000 images
+
+# After spending a day attempting, I was not able to correctly shape and reshape 
+# the mnist data coming from torch to appropriately work with sklean.KMeans.  
+# Given that the assignment asks us to focus on the accuracy of predictions, in 
+# the interest of time I decided to import the same MNIST data using Keras.
+from keras.datasets import mnist as kmnist
+
+(X_train, y_train), (X_test, y_test) = kmnist.load_data()
+# X_train, X_val = random_split(X_train, [int(m-m*0.2), int(m*0.2)])
+# X_val = X_train[48000:]
+# X_train = X_train[:48000]
+ # normalize X to be between 0 and 1
+X_train = X_train.astype('float32') / 255.
+X_train = X_train.reshape((len(X_train), -1))
+total_cluster = len(np.unique(y_train)) # should be 10
+kmeans = MiniBatchKMeans(n_clusters=total_cluster, random_state=0)
+kmeans.fit(X_train)
+# kmeans = kmeans.predict(X_train)
+kmeans.labels_
 
 
+ref_labels = retrieve_info(kmeans.labels_, y_train)
+number_labels = np.random.rand(len(kmeans.labels_))
+# github copilot suggestion
+for i in range(len(kmeans.labels_)):
+    number_labels[i] = ref_labels[kmeans.labels_[i]]
 
+predicted_labels = number_labels[:count].astype(int)
+print('Predicted labels: ', predicted_labels)
+print('Actual labels: ', actual_labels)
+print('Accuracy: ', accuracy_score(actual_labels, predicted_labels))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# EOF 
