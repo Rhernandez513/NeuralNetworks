2b877eadc1ceea1e51964fb20cb98fc10415d386
Robert David Hernandez <rherna70@uic.edu>
2022-11-10 15:35:01 -0600

    basic data ingestion and pre-processing

--- hmwk/hmwk7/main.py
+++ hmwk/hmwk7/main.py
@@ -2,25 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
-
 # EOS char => #
 
-# a = [1, 0, 0, 
 
-def get_a():
-	vec = np.zeros(27)	
-	vec[0] = 1
-	return vec
-
+def get_names():
+	with open("names.txt") as f:
+		lines = f.readlines()
+	for idx, val in enumerate(lines):
+		lines[idx] = val.lower()
+	return lines
 
 def main():
-	a_as_vec = get_a()
-	print("len: " + str(len(a_as_vec)))
-	print(a_as_vec)
+	# vector representation of the letter a
+	vec = np.zeros(27)	
+	vec[0] = 1
+
+	eos_char = np.zeros(27)
+	eos_char[len(vec) - 1] = 1
+
+	names = get_names()
+
+	print("a: " + str(len(vec)))
+	print("eos: " + str(len(eos_char)))
+	print(vec)
+	print(eos_char)
+	print(names)
+
 
 
 if __name__ == "__main__":
 	main()
 
 # EOF
-
