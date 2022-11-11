6d723c8db9a6be08810c4c373dc3c1dac910b09b
Robert David Hernandez <rherna70@uic.edu>
2022-11-10 19:13:02 -0600

    kinda sorta hot encoding works

--- hmwk/hmwk7/main.py
+++ hmwk/hmwk7/main.py
@@ -1,8 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # EOS char => #
+
+# define a recurrent neural network 
+def RNN(x, weights, biases):
+	# use the RNN to predict the next letter in the sequence
+
+	# ok so we are going to have to take in the input names, reshape to 1xN and then feed into the RNN
+
+	# we will have to design a simple RNN that takes in a sequence of letters and predicts the next letter in the sequence
+
+	pass
+
+# ok so how are we going to measuire the loss of the RNN? 
+# copilot suggestion => potentially use the pytorch cross entropy loss function
+
+class BasicLSTM(nn.Module):
+	def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+		# github copilot suggestion
+		super(BasicLSTM, self).__init__()
+		self.num_classes = num_classes
+		self.num_layers = num_layers
+		self.input_size = input_size
+		self.hidden_size = hidden_size
+		# I figure we will need this one, should be 27 for alpahebet plus EOS
+		# self.seq_length = 27
+		self.seq_length = seq_length
+		self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+		# self.fc_1 = nn.Linear(hidden_size, 64)
+		# self.fc_2 = nn.Linear(64, num_classes)
+		# starting with a smaller number of nodes to keep training time down
+		self.fc_1 = nn.Linear(hidden_size, 16)
+		self.fc_2 = nn.Linear(16, num_classes)
+
+		# this one wasn't suggested by copilot, not needed? an ReLu can't hurt though
+		self.relu = nn.ReLU()
+
+	def fwd(self, x):
+		# github copilot suggestion
+		# Set initial states
+		h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+		c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+		# Forward propagate LSTM
+		out, _ = self.lstm(x, (h0, c0)) #lstm 
+		# do we not need (hn, cn)? todo look up the interface , todo look up what hn stands for 
+		# out, (hn, cn) = self.lstm(x, (h0, c0)) #lstm 
+		# out = self.relu(hn)
+		out = self.fc_1(out)
+		out = self.relu(out)
+		out = self.fc_2(out)
+		return out
+
+	pass 
 
 
 def get_names():
@@ -11,6 +66,40 @@ def get_names():
 	for idx, val in enumerate(lines):
 		lines[idx] = val.lower()
 	return lines
+
+# todo generate this programatically so we don't have a huge sparse matrix in source
+alphas = {
+	'a'	: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'b'	: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'c'	: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'd'	: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'e'	: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'f'	: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'g'	: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'h'	: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'i'	: [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'j'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'k'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'l'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'm'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'n'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'o'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'p'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'q'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	'r'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+	's'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
+	't'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], 
+	'u'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], 
+	'v'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 
+	'w'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], 
+	'x'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 
+	'y'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
+	'z'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], 
+	'#'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+}
+
+def get_vector_representation(char) -> np.array:
+	return np.array(alphas[char])
 
 def main():
 	# vector representation of the letter a
@@ -21,16 +110,44 @@ def main():
 	eos_char[len(vec) - 1] = 1
 
 	names = get_names()
+	print(str(len(names)))
 
 	print("a: " + str(len(vec)))
 	print("eos: " + str(len(eos_char)))
 	print(vec)
 	print(eos_char)
-	print(names)
+	# print(names)
+
+	# x = names
+	length = len(names)
+	# x_train = names[0:0.8*length]
+	# x_test = names[0.8*length:length]
+	ltsm = BasicLSTM(27, 27, 27, 1, 27)	
+
+	a_name_as_vec = []
+	for char in names[0]:
+		if char == '\n':
+			a_name_as_vec.append(eos_char)
+			continue
+		a_name_as_vec.append(get_vector_representation(char))
+
+	if len(a_name_as_vec) < 11:
+		for i in range(10 - len(a_name_as_vec)):
+			a_name_as_vec.append(eos_char)
+
+	print(names[0])
+	print(a_name_as_vec)
+	print(len(a_name_as_vec))
+
+	# ok so we need to convert the names into a vector representation
+
+
+# def alphabet_to_vec(name) -> np.array:
+	# hot encode the name into a vector representation
+	# we will have to pad the name with EOS chars to make it 27 chars long
 
 
 
-if __name__ == "__main__":
-	main()
+main()
 
 # EOF
