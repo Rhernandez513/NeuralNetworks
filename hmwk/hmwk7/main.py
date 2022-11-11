0935a507282f0b87e9e92ef6e49cf1c0494f24a5
Robert David Hernandez <rherna70@uic.edu>
2022-11-11 16:02:58 -0600

    converted strings to hot encoded vectors

--- hmwk/hmwk7/main.py
+++ hmwk/hmwk7/main.py
@@ -38,7 +38,7 @@ class BasicLSTM(nn.Module):
 		self.fc_1 = nn.Linear(hidden_size, 16)
 		self.fc_2 = nn.Linear(16, num_classes)
 
-		# this one wasn't suggested by copilot, not needed? an ReLu can't hurt though
+		# this one wasn't suggested by copilot, not needed? a ReLu can't hurt though..
 		self.relu = nn.ReLU()
 
 	def fwd(self, x):
@@ -48,10 +48,10 @@ class BasicLSTM(nn.Module):
 		c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
 
 		# Forward propagate LSTM
-		out, _ = self.lstm(x, (h0, c0)) #lstm 
-		# do we not need (hn, cn)? todo look up the interface , todo look up what hn stands for 
-		# out, (hn, cn) = self.lstm(x, (h0, c0)) #lstm 
-		# out = self.relu(hn)
+		# out, _ = self.lstm(x, (h0, c0)) #lstm 
+		# do we not need cn? 
+		out, (hn, cn) = self.lstm(x, (h0, c0)) #lstm 
+		out = self.relu(hn)
 		out = self.fc_1(out)
 		out = self.relu(out)
 		out = self.fc_2(out)
@@ -60,7 +60,7 @@ class BasicLSTM(nn.Module):
 	pass 
 
 
-def get_names():
+def get_names_from_file():
 	with open("names.txt") as f:
 		lines = f.readlines()
 	for idx, val in enumerate(lines):
@@ -98,53 +98,34 @@ alphas = {
 	'#'	: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 }
 
-def get_vector_representation(char) -> np.array:
-	return np.array(alphas[char])
+def name_to_vector_repr(name: str) -> np.array:
+	eos_char = np.array(alphas['#'])
+
+	vector_repr = []
+	for char in name:
+		if char == '\n':
+			vector_repr.append(eos_char)
+			continue
+		vector_repr.append(np.array(alphas[char]))
+
+	if len(vector_repr) < 11:
+		for i in range(11 - len(vector_repr)):
+			vector_repr.append(eos_char)
+
+	return np.array(vector_repr)
 
 def main():
 	# vector representation of the letter a
-	vec = np.zeros(27)	
-	vec[0] = 1
 
-	eos_char = np.zeros(27)
-	eos_char[len(vec) - 1] = 1
+	names = get_names_from_file()
 
-	names = get_names()
-	print(str(len(names)))
+	names_vector = np.array([name_to_vector_repr(name) for name in names])
 
-	print("a: " + str(len(vec)))
-	print("eos: " + str(len(eos_char)))
-	print(vec)
-	print(eos_char)
-	# print(names)
-
-	# x = names
-	length = len(names)
+	# x = names_vector
 	# x_train = names[0:0.8*length]
 	# x_test = names[0.8*length:length]
-	ltsm = BasicLSTM(27, 27, 27, 1, 27)	
-
-	a_name_as_vec = []
-	for char in names[0]:
-		if char == '\n':
-			a_name_as_vec.append(eos_char)
-			continue
-		a_name_as_vec.append(get_vector_representation(char))
-
-	if len(a_name_as_vec) < 11:
-		for i in range(10 - len(a_name_as_vec)):
-			a_name_as_vec.append(eos_char)
-
-	print(names[0])
-	print(a_name_as_vec)
-	print(len(a_name_as_vec))
-
-	# ok so we need to convert the names into a vector representation
-
-
-# def alphabet_to_vec(name) -> np.array:
-	# hot encode the name into a vector representation
-	# we will have to pad the name with EOS chars to make it 27 chars long
+	# def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+	ltsm = BasicLSTM(27, 27, 27, 11, 11)	
 
 
 
